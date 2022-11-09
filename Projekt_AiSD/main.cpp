90cf415a8e39658ddece22c7d92d3a13558e6792
Karolinamag <117656056+Karolinamag@users.noreply.github.com>
2022-11-09 14:42:34 +0100

    Tablice
    nie działa

--- Projekt_AiSD/main.cpp
+++ Projekt_AiSD/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    int tab;
-    [-10,5,8,-4,1,3,0,-7]
-    
-    if()
-    return 0;
+int* GenerowanieTablic (int ListaLiczb[]) {
+
+srand((unsigned) time(0));
+
+for (int j=0; j<10; j++){
+    cout<<(1+rand() % 100 )<<endl;
+    ListaLiczb[j]=(1+rand() % 100 );
+    cout<<ListaLiczb[j]<<endl;
 }
+
+    cout<<ListaLiczb<<endl;
+  return ListaLiczb;
+}
+
+
