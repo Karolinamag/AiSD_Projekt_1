1ce0326591a3f37d6a2d87e6e4d8da64820e0c47
Karolinamag <117656056+Karolinamag@users.noreply.github.com>
2022-11-12 10:06:58 +0100

    Przesuwanie liczb ujemnych na koniec
    Stworzyłam algorytm na podstawie pomysłu przesuwania liczb w prawo, miał po prostu brać liczbę ujemną i przesuwać na koniec tabeli. Po testach zorientowałam się, że przez to, że po każdej znalezionej liczbie tabela się tasuje, algorytm nie ma prawa zadziałać.

--- Projekt_AiSD/main.cpp
+++ Projekt_AiSD/main.cpp
@@ -9,13 +9,34 @@ srand((unsigned) time(0));
 for (int j=0; j<10; j++){
     ListaLiczb[j]=((rand() % 20)-10 );
 }
+
+cout<<"Wygenerowana Tabica = [ ";
+for (int j=0; j<10; j++){
+    cout <<ListaLiczb[j]<<" ";
+}
+cout<<"]"<<endl;
 }
 
 void GlownaFunkcjaPrzesuwania (int ListaLiczb[]){
 
+int Temp;
+
 for (int j=0; j<10; j++){
-    if(ListaLiczb[j]<0){cout<<ListaLiczb[j]<<endl;}
+    if(ListaLiczb[j]<0){
+        for(int p=j; p<9; p++){
+            cout<<"Zamieniam "<<ListaLiczb[p]<<" z "<<ListaLiczb[p+1]<<endl;
+            Temp = ListaLiczb[p];
+            ListaLiczb[p]=ListaLiczb[p+1];
+            ListaLiczb[p+1]= Temp;
+        }
+    }
 }
+
+cout<<"Tabica po przesunieciu = [ ";
+for (int j=0; j<10; j++){
+    cout <<ListaLiczb[j]<<" ";
+}
+cout<<"]"<<endl;
 
 
 }
@@ -25,15 +46,10 @@ int main()
     int Tablica[10];
     GenerowanieTablic(Tablica);
 
-cout<<"Wygenerowana Tabica = [ ";
-for (int j=0; j<10; j++){
-    cout <<Tablica[j]<<" ";
-}
-cout<<"]"<<endl;
 
-
-GlownaFunkcjaPrzesuwania(Tablica);
+    GlownaFunkcjaPrzesuwania(Tablica);
 
 
     return 0;
 }
+
